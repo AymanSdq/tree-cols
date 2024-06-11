2ff1d0c8e0a1e85d6cd6dfe632e7f6cd0f200655
AymanSdq <sedqiayman02@gmail.com>
2024-06-11 11:08:42 +0100

    Desktop design end

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* Container of all the section */}
+      <main className="w-full h-screen flex justify-center items-center">
+        <section className="w-[80%] flex justify-between rounded-lg ">
+          {/* First section */}
+          <div className="flex rounded-l-lg gap-10 flex-col bg-brightOrange w-full text-white px-16 py-16 justify-center">
+            <img className="w-[80px]" src="/icon-sedans.svg" alt="Sedans Icons" />
+            <h1 className=" text-4xl bigshoulder uppercase">Sedans</h1>
+            <p className="lexend">Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
+            or on your next road trip.</p>
+            <a href="#" className="lexend mt-12 w-1/2 bg-white rounded-full py-3 text-brightOrange text-center">Learn More</a>
+          </div>
+          {/* Second section */}
+          <div className="flex gap-10 flex-col bg-darkCyan w-full text-white px-16 py-16 justify-center">
+            <img className="w-[80px]" src="/icon-suvs.svg" alt="Sedans Icons" />
+            <h1 className=" text-4xl bigshoulder uppercase">SUVs</h1>
+            <p className="lexend">Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
+            and off-road adventures.</p>
+            <a href="#" className="lexend mt-12 w-1/2 bg-white rounded-full py-3 text-darkCyan text-center">Learn More</a>
+          </div>
+          {/* Third section */}
+          <div className="flex gap-10 rounded-r-lg flex-col bg-veryDarkCyan w-full text-white px-16 py-16 justify-center">
+            <img className="w-[80px]" src="/icon-luxury.svg" alt="Sedans Icons" />
+            <h1 className=" text-4xl bigshoulder uppercase">Luxury</h1>
+            <p className="lexend">Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
+            rental and arrive in style.</p>
+            <a href="#" className="lexend mt-12 w-1/2 bg-white rounded-full py-3 text-veryDarkCyan text-center">Learn More</a>
+          </div>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brightOrange" : "hsl(31, 77%, 52%)",
+        "darkCyan" : "hsl(184, 100%, 22%)",
+        "veryDarkCyan" : "hsl(179, 100%, 13%)"
+      }
+    },
   },
   plugins: [],
 }
